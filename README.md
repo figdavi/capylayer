9a8960d0017a84def2dfe3103fd48daf739c7f71
figdavi <figueiredodavi@outlook.com>
2024-11-20 10:31:13 -0300

    Add context to README, change symbol layer from switvh to hold with capslock
    Changed every string/key name to variables to its easier to change presets and now the second symbol layer is activated when the modifier key is being held as opposed to on/off switch.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # keymapper
-Add a symbol layer to keyboard
+Simple python script, using [keyboard](https://github.com/boppreh/keyboard/) python library, that adds a "second" layer to keyboard using CapsLock key. 
+
+For now, I've only mapped wasd keys to up, left, down, right, respectively, to the second layer (when CapsLock is on). This makes it easier to navigate text while staying in the home row of your keyboard.
+
+See this [article](https://dreymar.colemak.org/layers-extend.html) for more explanation and showcase of common layer presets (Which I plan to add in the future).
--- main.py
+++ main.py
@@ -1,59 +1,31 @@
 import keyboard
-from win32api import GetKeyState
-from win32con import VK_CAPITAL
-
-# Variables containing '_was_'/'_last_' store the key value
-#  assigned *last time* its state was checked, so it is the contrary
-#  of current's state.
 
 
-symbol_layer_active = None
-caps_lock_pressed = None
-quit_command = 'ctrl+shift+caps lock'
-caps_lock_key_name = 'caps lock'
+key_mappings = {
+    'w': 'up',
+    'a': 'left',
+    's': 'down',
+    'd': 'right'
+}
+
+modifier_key_name = 'caps lock'
+quit_command = 'ctrl+shift+' + modifier_key_name
 
 
 def activate_symbol_layer():
-    keyboard.remap_key('w', 'up')
-    keyboard.remap_key('a', 'left')
-    keyboard.remap_key('s', 'down')
-    keyboard.remap_key('d', 'right')
+    for key_src, key_dst in key_mappings.items():
+        keyboard.remap_key(key_src, key_dst)
 
 def deactivate_symbol_layer():
-    keyboard.unremap_key('w')
-    keyboard.unremap_key('a')
-    keyboard.unremap_key('s')
-    keyboard.unremap_key('d')
+    for key in key_mappings.keys():
+        keyboard.unremap_key(key)
 
-def handle_caps_lock(event):
-    global symbol_layer_active
-    global caps_lock_pressed
-
-    if event.event_type == keyboard.KEY_DOWN and not caps_lock_pressed:
-        caps_lock_pressed = True
-
-        if not symbol_layer_active:
-            activate_symbol_layer()
-            symbol_layer_active = True
-        else:
-            deactivate_symbol_layer() 
-            symbol_layer_active = False
-
-    elif event.event_type == keyboard.KEY_UP:
-        caps_lock_pressed = False 
-
-def onload():
-    global symbol_layer_active
-    global caps_lock_pressed
-
-    symbol_layer_active = bool(GetKeyState(VK_CAPITAL))
-    caps_lock_pressed = False
-
-    if symbol_layer_active:
+def handle_modifier_key(event):
+    if event.event_type == keyboard.KEY_DOWN and keyboard.is_pressed(modifier_key_name):
         activate_symbol_layer()
+    elif event.event_type == keyboard.KEY_UP:
+        deactivate_symbol_layer() 
+        
 
-
-onload()
-
-keyboard.hook_key(caps_lock_key_name, handle_caps_lock)
+keyboard.hook_key(modifier_key_name, handle_modifier_key)
 keyboard.wait(quit_command)
